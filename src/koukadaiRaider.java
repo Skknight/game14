c592eac684e5a1764aecd30af9ea47b5c8c81424
Daniel <francis_cader@hotmail.com>
2018-11-09 14:48:30 +0900

    Update koukadaiRaider.java

--- src/koukadaiRaider.java
+++ src/koukadaiRaider.java
@@ -1,14 +1,64 @@
 import com.sun.media.jfxmedia.events.PlayerStateEvent;
 
-/**
- * Created by takumi on 2018/11/02.
- */
-// TEST
-public class koukadaiRaider {
+import java.util.Scanner;
+
+public class lol {
+	public static final Scanner USERINPUT = new Scanner(System.in);
+	
     public static void main(String[] args) {
         System.out.println("ゲーム開始");
-        Map map = new Map();    //ダンジョンの初期化
-        Move mv = new Move();
-        Battle bt = new Battle();
+        mainMenu();
+        //Map map = new Map();
+       // Move mv = new Move();
+        //Battle bt = new Battle();
+    }
+    public static void mainMenu() {
+    	welcome();
+        boolean status = false;
+        do {
+            String selection = USERINPUT.nextLine();
+            switch (selection) {
+                case "1":
+                    newGame();
+                    status = true;
+                    break;
+                case "2":
+                    rule();
+                    status = true;
+                    break;    
+                case "3":
+                    System.exit(0);
+            }
+
+        } while (status == false);
+    }
+    
+    public static void welcome() {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("Welcome!");
+        System.out.println("-----------------------");
+        System.out.println("       MAIN MENU       ");
+        System.out.println("_______________________");
+        System.out.println();
+        System.out.println(" 1.      New Game        ");
+        System.out.println();
+        System.out.println(" 2.     Rule        ");
+        System.out.println();
+        System.out.println(" 3.     Exit        ");
+        System.out.println();
+        System.out.print(" Make a Selection: ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+    
+    public static void newGame() {
+    	//newGame
+    	System.out.println("GAME");
+    }
+    public static void rule() {
+    	//rule
+    	System.out.println("RULES");
     }
 }
